da0ae9971ac969a6d56f50a0ad52e1bed0a1f823
Viswasai Gollamudi <36977407+Viswasai-G@users.noreply.github.com>
2019-03-07 12:53:24 -0800

    Update Application.java

--- Source Code/Application.java
+++ Source Code/Application.java
@@ -11,7 +11,9 @@ public class Application {
 
 
     public static void main(String[] args) {
-
+        
+        //Create the database and run the application
+        
         Database firstdb = new Database();
         firstdb.createTable();
         SpringApplication.run(Application.class, args);
