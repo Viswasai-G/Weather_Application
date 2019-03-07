8800b842b749f47f18e7c99cb12f8a1a20c02770
Viswasai Gollamudi <36977407+Viswasai-G@users.noreply.github.com>
2019-03-07 13:24:45 -0800

    Update main_Controller2.java

--- Source Code/main_Controller2.java
+++ Source Code/main_Controller2.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
+//Springboot controller for returningUser.html page. Returns index webpage when '/returningUser' is accessed by user.
 @Controller
 public class main_Controller2 {
+    //Create a new database object to connect with the DB.
     Database dbObject = new Database();
 
+    //newUserReg webpage with user class attributes linked to the form input tags.
     @GetMapping("/returningUser")
     public String greeting( Model model) {
         model.addAttribute("user", new User());
@@ -22,23 +24,28 @@ public class main_Controller2 {
 
     }
 
+    //After user enters data, we validate the given data. Return the same page if error exists in data entered. Else, return results.
     @PostMapping("/returningUser")
     public String result(@Valid User user, BindingResult bindingResult){
         String Email_val = user.getEmailid();
 
+        //Return same page if error in data exists
         if(bindingResult.hasErrors()){
             if(bindingResult.hasFieldErrors("emailid")){
                 return "returningUser";
             }
         }
+        
+        //Read data from DB using the primary key 'Email' entered by the user.
         User temp_user = dbObject.readData("weatherDB", Email_val);
-
+    
+        //Set the outcome to user object so that the webpage can access the results.
         user.setName(temp_user.getName());
         user.setZip(temp_user.getZip());
         user.setWeather(temp_user.getWeather());
 /*
 
-    To get Updated results....
+    To get Updated weather results, Connect to the API with the zip information stored in DB and get results....
 
         int zipSearcher = temp_user.getZip();
         String url = "https://api.openweathermap.org/data/2.5/weather?zip="+zipSearcher+",us&units=imperial&APPID=813952d1da48dc8341125740b7112339";
